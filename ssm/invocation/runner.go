fb1efbdc38591a7f6229464e74b0b6bb2c1da31e
sendqueery <47192407+sendqueery@users.noreply.github.com>
2020-08-05 13:24:23 -0400

    Remove unused function and add runner tests/mocks

--- ssm/invocation/runner.go
+++ ssm/invocation/runner.go
@@ -2,7 +2,6 @@ package invocation
 
 import (
 	"fmt"
-	"time"
 
 	"github.com/aws/aws-sdk-go/service/ssm"
 	"github.com/aws/aws-sdk-go/service/ssm/ssmiface"
@@ -19,34 +18,7 @@ func RunSSMCommand(session ssmiface.SSMAPI, input *ssm.SendCommandInput, dryRunF
 	return
 }
 
-func GetTargets(client ssmiface.SSMAPI, commandID *string) (targets []*string, err error) {
-	var out *ssm.ListCommandInvocationsOutput
-
-	// Try a few times to get the invocation data, because it takes a little bit to have any information
-	for i := 0; i < 3; i++ {
-		time.Sleep(1 * time.Second)
-		if out, err = client.ListCommandInvocations(&ssm.ListCommandInvocationsInput{
-			CommandId: commandID,
-		}); err != nil {
-			return nil, err
-		}
-
-		if len(out.CommandInvocations) > 0 {
-			break
-		}
-	}
-
-	if len(out.CommandInvocations) == 0 {
-		return nil, fmt.Errorf("API response contained no invocations")
-	}
-
-	for _, inv := range out.CommandInvocations {
-		targets = append(targets, inv.InstanceId)
-	}
-
-	return targets, nil
-}
-
+// GetResult fetches the output of an invocation using the associated command and instance IDs
 func GetResult(client ssmiface.SSMAPI, commandID *string, instanceID *string, gci chan *ssm.GetCommandInvocationOutput, ec chan error) {
 	status, err := client.GetCommandInvocation(&ssm.GetCommandInvocationInput{
 		CommandId:  commandID,
--- ssm/invocation/runner_test.go
+++ ssm/invocation/runner_test.go
@@ -1,77 +1,78 @@
 package invocation
 
 import (
-	"fmt"
-	"os"
 	"testing"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/ssm"
-	"github.com/stretchr/testify/assert"
-
 	mocks "github.com/disneystreaming/ssm-helpers/testing"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestRunSSMCommand(t *testing.T) {
 	assert := assert.New(t)
-	resultChan := make(chan *ssm.SendCommandOutput)
-	errChan := make(chan error)
 
 	// Set up our mocks for testing RunSSMCommand()
 	mockSvc := &mocks.MockSSMClient{}
-	mockParams := &RunShellScriptParameters{
-		"commands":         aws.StringSlice([]string{"echo foo", "uname -a"}),
-		"executionTimeout": aws.StringSlice([]string{"600"}),
+	mockInput := &ssm.SendCommandInput{
+		InstanceIds:  aws.StringSlice([]string{"i-123", "i-456"}),
+		DocumentName: aws.String("AWS-RunShellScript"),
+		Parameters: map[string][]*string{
+			/*
+				ssm.SendCommandInput objects require parameters for the DocumentName chosen
+
+				For AWS-RunShellScript, the only required parameter is "commands",
+				which is the shell command to be executed on the target. To emulate
+				the original script, we also set "executionTimeout" to 10 minutes.
+			*/
+			"commands":         aws.StringSlice([]string{"uname -a", "hostname"}),
+			"executionTimeout": aws.StringSlice([]string{"600"}),
+		},
 	}
 
 	t.Run("dry run flag false", func(t *testing.T) {
-		go RunSSMCommand(mockSvc, mockParams, false, resultChan, errChan, "i-123456", "i-654321")
-		output, _ := <-resultChan, <-errChan
+		output, err := RunSSMCommand(mockSvc, mockInput, false)
 
-		// Do we have two instance IDs in our output?
-		assert.Len(output.Command.InstanceIds, 2, "Incorrect nunber of instances passed to RunSSMCommand()")
-
-		// Did we submit two separate commands to be executed by this document?
-		assert.Len(output.Command.Parameters["commands"], 2, "Incorrect number of commands passed from mockParams object into SendCommandInput")
+		assert.NoError(err)
+		assert.NotNil(output)
 	})
 
 	t.Run("dry run flag true", func(t *testing.T) {
-		go RunSSMCommand(mockSvc, mockParams, true, resultChan, errChan, "i-123456", "i-654321")
-		output, _ := <-resultChan, <-errChan
+		output, err := RunSSMCommand(mockSvc, mockInput, true)
 
+		assert.NoError(err)
 		assert.Nil(output, "Dry run flag enabled, should not have received any output")
 	})
 }
 
-func TestGetCommandInvocationResult(t *testing.T) {
+func TestGetResult(t *testing.T) {
 	assert := assert.New(t)
 	mockSvc := &mocks.MockSSMClient{}
 
-	// Mock two batches with two instances each
-	jobs := []*ssm.SendCommandOutput{
-		{
-			Command: &ssm.Command{
-				CommandId:    aws.String("123456123456123456123456123456123456"),
-				DocumentName: aws.String("AWS-RunShellScript"),
-				InstanceIds:  aws.StringSlice([]string{"i-12345", "i-67890"}),
-			},
-		},
-		{
-			Command: &ssm.Command{
-				CommandId:    aws.String("abcdefabcdefabcdefabcdefabcdefabcdef"),
-				DocumentName: aws.String("AWS-RunShellScript"),
-				InstanceIds:  aws.StringSlice([]string{"i-54321", "i-09876"}),
-			},
-		},
-	}
-	// So we can track state for each mock invocation in order to test handling of different return values
-	for _, v := range jobs {
-		for _, v := range v.Command.InstanceIds {
-			os.Setenv(fmt.Sprintf("%s-trycount", *v), "0")
-		}
-	}
-	results, err := GetCommandInvocationResult(mockSvc, jobs...)
+	successCmd, badCmd, mockInstance :=
+		aws.String("success-id"), aws.String("bad-id"), aws.String("i-123")
 
-	assert.Nilf(err, "Unexpected error returned from GetCommandInvocationResult\n%v", err)
-	assert.Lenf(results, 4, "Incorrect number of invocation results returned; got %d, expected 4", len(results))
+	oc := make(chan *ssm.GetCommandInvocationOutput)
+	ec := make(chan error)
+
+	t.Run("valid ID", func(t *testing.T) {
+		go GetResult(mockSvc, successCmd, mockInstance, oc, ec)
+		select {
+		case result := <-oc:
+			assert.Equal("success-id", *result.CommandId)
+		case err := <-ec:
+			assert.Empty(err)
+		}
+
+	})
+
+	t.Run("invalid ID", func(t *testing.T) {
+		go GetResult(mockSvc, badCmd, mockInstance, oc, ec)
+		select {
+		case result := <-oc:
+			assert.Empty(result)
+		case err := <-ec:
+			assert.Error(err)
+		}
+	})
 }
